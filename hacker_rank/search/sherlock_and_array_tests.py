5e0a9dc65b413d97b89027c70798ad4f22b43ac8
caterina <caterina.lorente@gmail.com>
2016-01-10 14:08:49 +0100

    Added unit tests for new elegant way

--- hacker_rank/search/sherlock_and_array_tests.py
+++ hacker_rank/search/sherlock_and_array_tests.py
@@ -1,15 +1,23 @@
 import unittest
-import sherlock_and_array
+from mooc.hacker_rank.search.sherlock_and_array import sherlock_and_array_elegant, sherlock_and_array_brute_force
 
 
 class MyTest(unittest.TestCase):
     def test(self):
-        self.assertEqual(sherlock_and_array(1,[1]), "YES")
-        self.assertEqual(sherlock_and_array(2,[1,0]), "YES")
-        self.assertEqual(sherlock_and_array(2,[2,1]), "NO")
-        self.assertEqual(sherlock_and_array(3,[55,1,55]), "YES")
-        self.assertEqual(sherlock_and_array(3,[1,-1,32]), "YES")
-        self.assertEqual(sherlock_and_array(3,[55,1,32]), "NO")
-        self.assertEqual(sherlock_and_array(4, [1,2,3,3]), "YES")
+        self.assertEqual(sherlock_and_array_brute_force("1\n1\n1"), "YES")
+        self.assertEqual(sherlock_and_array_brute_force("1\n2\n1 0"), "YES")
+        self.assertEqual(sherlock_and_array_brute_force("1\n2\n2 1"), "NO")
+        self.assertEqual(sherlock_and_array_brute_force("1\n3\n55 1 55"), "YES")
+        self.assertEqual(sherlock_and_array_brute_force("1\n3\n1 -1 32"), "YES")
+        self.assertEqual(sherlock_and_array_brute_force("1\n3\n55 1 32"), "NO")
+        self.assertEqual(sherlock_and_array_brute_force("1\n4\n1 2 3 3"), "YES")
+
+        self.assertEqual(sherlock_and_array_elegant("1\n1\n1"), "YES")
+        self.assertEqual(sherlock_and_array_elegant("1\n2\n1 0"), "YES")
+        self.assertEqual(sherlock_and_array_elegant("1\n2\n2 1"), "NO")
+        self.assertEqual(sherlock_and_array_elegant("1\n3\n55 1 55"), "YES")
+        self.assertEqual(sherlock_and_array_elegant("1\n3\n1 -1 32"), "YES")
+        self.assertEqual(sherlock_and_array_elegant("1\n3\n55 1 32"), "NO")
+        self.assertEqual(sherlock_and_array_elegant("1\n4\n1 2 3 3"), "YES")
 
 unittest.main()
